0ea9e674fc7482a23bd33e403ee709c622bdc272
Abhijit <abhijitreddybutti@gmail.com>
2026-04-28 11:32:08 -0400

    Added Elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,16 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in
+ * 
+ * @author Abhijit
+ * @version Apr 28 2026
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
