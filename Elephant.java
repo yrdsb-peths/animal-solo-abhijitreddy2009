9600549ecbd87c91f7ca6f3805100566584d2bc6
Abhijit <abhijitreddybutti@gmail.com>
2026-05-04 16:04:09 -0400

    added background, sounds, and elephant images

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephant.mp3");
+    
+    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    
+    
+    public Elephant()
+    {
+        setImage(idle);
+    }
+    
     public void act()
     {
         
@@ -36,6 +42,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
+            elephantSound.play();
         }
     }
 }
